87192f4cf52f110f63fd66b8456106fe4a58c418
Josh <josh.bondy@codekiwi.com>
2024-07-30 12:31:14 +1200

    angels

--- components/pages/Mutateds.tsx
+++ components/pages/Mutateds.tsx
@@ -224,7 +224,7 @@ const Mutateds = ({ summary }: { summary: any }) => {
                                                 </label>
                                                 <label className="cursor-pointer label">
                                                     <span className="text-white">
-                                                        Pay 34A in <span className="text-secondary">$Angels</span>
+                                                        Pay 33A in <span className="text-secondary">$Angels</span>
                                                     </span>
                                                     <input
                                                         type="checkbox"
